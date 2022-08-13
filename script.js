dde9d991faa82ceb1f2e9576126122f466216c6f
Ahmed-Nader64 <100669445+Omar-Nader64@users.noreply.github.com>
2022-08-13 14:46:58 +0300

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+alert("مرحبا بك في موقعي الذي سيحسب درجتك !");
+
+let grade = prompt("اكتب درجتك بالأرقام");
+console.log(grade);
+
+if(grade >= 90){
+console.log("%cلقد حصلت على امتياز 🥳","color:red;");
+
+}else if(grade >= 80){
+console.log("%cلقد حصلت على جيد جداً🤩","color:red;");
+
+}else if(grade >= 70){
+console.log("%cلقد حصلت على جيد🙂","color:red;");
+
+}else if(grade >= 60){
+console.log("%cلقد حصلت على مقبول😕","color:red;");
+
+}else if(grade >= 50){
+console.log("%cلقد حصلت على جيد🙂","color:red;");
+
+}else{
+console.log("%cراسب💔","color:red;")
+}
+
+
+// javascript not java 
+
